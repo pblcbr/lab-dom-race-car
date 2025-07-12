46ac477286036e1b7485aef896b39c15c1a3c90f
Pablo Cabrera <pablo.cabrera.lopez@gmail.com>
2025-07-12 17:11:49 +0200

    Solved lab

--- js/game.js
+++ js/game.js
@@ -1,3 +1,74 @@
 class Game {
-    // code to be added
-}
+    constructor() {
+        this.startScreen = document.querySelector("#game-intro");
+        this.gameScreen = document.querySelector("#game-screen");
+        this.gameEndScreen = document.querySelector("#game-end");
+        this.player = new Player (this.gameScreen, 200, 500, 100, 150, "./images/car.png");
+        this.height = 600;
+        this.width = 500;
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+        this.gameIntervalId;
+        this.gameLoopFrecuency = 1000/60;
+    }
+
+    start() {
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
+        this.startScreen.style.display = "none";
+        this.gameScreen.style.display = "block";
+        this.gameIntervalId = setInterval(() => {
+            this.gameLoop()
+        }, this.gameLoopFrecuency);
+    }
+
+    gameLoop() {
+        this.update()
+        if (this.gameIsOver === true){
+            clearInterval(this.gameIntervalId)
+        }
+
+    }
+
+    update() {
+        this.player.move()
+
+        for (let i=0;i<this.obstacles.length;i++) {
+            const obstacle = this.obstacles[i];
+            obstacle.move();
+
+            if (this.player.didCollide(obstacle)) {
+                obstacle.element.remove();
+                this.obstacles.splice(i,1);
+                this.lives--;
+                document.getElementById("lives").textContent = this.lives;
+                i--;
+            }
+            else if (obstacle.top > this.height) {
+                this.score++;
+                obstacle.element.remove();
+                document.getElementById("score").textContent = this.score;
+                this.obstacles.splice(i,1);
+                i--;
+            }
+        }
+            if (this.lives === 0) {
+                this.endGame();
+            }
+
+            if (Math.random() > 0.98 && this.obstacles.length < 1) {
+                this.obstacles.push(new Obstacle(this.gameScreen));
+            }
+        }
+
+        endGame() {
+            this.player.element.remove();
+            this.obstacles.forEach(obstacle => obstacle.element.remove());
+            this.gameIsOver = true;
+            this.gameScreen.style.display = "none";
+            this.gameEndScreen.style.display = "block";
+        }
+
+    }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,62 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
   function startGame() {
+    game = new Game();
+    game.start();
     console.log("start game");
+  }
+
+  function handleKeyDown(event) {
+    const key = event.key;
+    const possibleKeystrokes = ["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"];
+
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -5;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -5;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 5;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 5;
+          break;
+      }
+    }
+  }
+  window.addEventListener("keydown", handleKeyDown);
+
+  window.addEventListener("keyup", function (event) {
+    const key = event.key;
+    switch (key) {
+      case "ArrowLeft":
+      case "ArrowRight":
+        game.player.directionX = 0;
+        break;
+      case "ArrowUp":
+      case "ArrowDown":
+        game.player.directionY = 0;
+        break;
+    }
+  });
+
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  });
+
+  function restartGame() {
+    location.reload();
   }
 };
